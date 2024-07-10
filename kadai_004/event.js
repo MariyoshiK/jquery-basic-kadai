fa555b9266052bf3ef6a85d9aa92b2d01fdadb0b
Kmary <166591688+MariyoshiK@users.noreply.github.com>
2024-07-10 14:41:45 +0900

    event.js 修正1回目

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,5 +1,8 @@
-$(function() {
+$(window).on('load', function(){
   console.log('loadイベントが発生しました');
+});
+
+$(function() {
   $(window).on('scroll', function(){
     console.log('scrollイベントが発生しました');
   });
